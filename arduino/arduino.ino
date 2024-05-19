00ba67b2acd329f4ff3800de7375cf91e1cc8e88
Ales Teska <ales.teska@teskalabs.com>
2024-05-19 15:41:27 +0200

    Update

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,15 +1,7 @@
 #include <AM2302-Sensor.h>
 
 /// Paramterization
-
-const int LED1 = 3;
-const int LED2 = 5;
-const int LED3 = 6;
-const int LED4 = 9;
-const int LED5 = 10;
-
 #define temperatureHumidityPin 7
-AM2302::AM2302_Sensor am2302 { temperatureHumidityPin };
 
 #define voltagePin A5
 
@@ -19,8 +11,7 @@ AM2302::AM2302_Sensor am2302 { temperatureHumidityPin };
 
 /////
 
-unsigned long previousMillis = 0;
-const long interval = 10000;  // interval at which to read (milliseconds)
+AM2302::AM2302_Sensor am2302 { temperatureHumidityPin };
 
 void setup() {
 	// inicializace sériové linky s rychlostí 9600 baud pro odesilani vysledku
@@ -55,33 +46,10 @@ void measure_temperature_humidity() {
 	}
 }
 
-void blink() {
-	float in, out;
-	for (in = 0; in < 6.283; in = in + 0.002)
-	{
-		out = sin(in) * 127.5 + 127.5;
-		analogWrite(LED1, out);
-		out = sin(in+0.628) * 127.5 + 127.5;
-		analogWrite(LED2, out);
-		out = sin(in+0.628*2) * 127.5 + 127.5;
-		analogWrite(LED3, out);
-		out = sin(in+0.628*3) * 127.5 + 127.5;
-		analogWrite(LED4, out);
-		out = sin(in+0.628*4) * 20.0 + 20.0;
-		analogWrite(LED5, out);
-	}
-}
-
 void loop() {
-	unsigned long currentMillis = millis();
+	measure_soil();
+	measure_voltage();
+	measure_temperature_humidity();
 
-	if (currentMillis - previousMillis >= interval) {
-		previousMillis = currentMillis;
-
-		measure_soil();
-		measure_voltage();
-		measure_temperature_humidity();
-		blink();
-	}
-	delay(1000);
+	delay(30 * 1000); // Measure once per 30 seconds
 }
